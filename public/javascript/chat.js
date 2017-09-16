3634fe4bead9340f1a15f20efea04e3ffcd688c3
sujank <sujanks6@gmail.com>
2017-09-16 12:00:15 +0000

    Changing ws to wss

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -4,34 +4,34 @@ app.controller('chatController',function($scope, $sce){
 
     $scope.messages = [
         { 
-    	sender:"BOT",
-    	text:"wat can i do for u?",
-    	time:"1:20pm"
-        },
-    	{ 
-    		sender:"USER",
-    		text:"r u ?",
-    		time:"1:05pm"
-    	},
-    	{
-    		sender:"BOT",
-    			text:"what?",
-    			time:"1:10pm"
-    	},
+    	// sender:"BOT",
+    	// text:"wat can i do for u?",
+    	// time:"1:20pm"
+     //    },
+    	// { 
+    	// 	sender:"USER",
+    	// 	text:"r u ?",
+    	// 	time:"1:05pm"
+    	// },
+    	// {
+    	// 	sender:"BOT",
+    	// 		text:"what?",
+    	// 		time:"1:10pm"
+    	// },
 
-    	{   
-    		sender:"USER",
-    		text:"r u?",
-    		time:"1:50pm"
-    	},
-    	{
-    		sender:"BOT",
-    		text:"hi",
-    		time:"1.50pm"
+    	// {   
+    	// 	sender:"USER",
+    	// 	text:"r u?",
+    	// 	time:"1:50pm"
+    	// },
+    	// {
+    	// 	sender:"BOT",
+    	// 	text:"hi",
+    	// 	time:"1.50pm"
 
     	}];
 
-var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+var  exampleSocket =  new  WebSocket("wss://swiftcode-sujan.herokuapp.com/chatSocket");
     exampleSocket.onmessage  =   function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date().toLocaleTimeString();
